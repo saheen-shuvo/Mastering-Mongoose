35de62c00bea4f9714f8602625d5be254bc3d126
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-05-17 02:12:14 +0600

    maxLength, trim validator

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -2,25 +2,25 @@ import { Schema, model } from "mongoose";
 import { Guardian, LocalGuardian, Student, UserName } from "./student/student.interface";
 
 const userNameSchema = new Schema<UserName>({
-    firstName: {type: String, required: [true, "First name is required"]},
-    middleName: {type: String},
-    lastName: {type: String, required: [true, "Last name is required"]}
+    firstName: {type: String, required: [true, "First name is required"], maxLength: [20, "First name should be less than 20 characters"], trim: true},
+    middleName: {type: String, maxLength: [20, "Middle name should be less than 20 characters"], trim: true},
+    lastName: {type: String, required: [true, "Last name is required"], trim: true}
 });
 
 const guardianSchema = new Schema<Guardian>({
-    fatherName: {type: String, required: [true, "Father name is required"]},
-    fatherOccupation: {type: String, required: [true, "Father occupation is required"]},
-    fatherContactNo: {type: String, required: [true, "Father contact number is required"]},
-    motherName: {type: String, required: [true, "Mother name is required"]},
-    motherOccupation: {type: String, required: [true, "Mother occupation is required"]},
-    motherContactNo: {type: String, required: [true, "Mother contact number is required"]}
+    fatherName: {type: String, required: [true, "Father name is required"], trim: true},
+    fatherOccupation: {type: String, required: [true, "Father occupation is required"], trim: true},
+    fatherContactNo: {type: String, required: [true, "Father contact number is required"], trim: true},
+    motherName: {type: String, required: [true, "Mother name is required"], trim: true},
+    motherOccupation: {type: String, required: [true, "Mother occupation is required"], trim: true},
+    motherContactNo: {type: String, required: [true, "Mother contact number is required"], trim: true}
 });
 
 const localGuardianSchema = new Schema<LocalGuardian>({
-    name: {type: String, required: [true, "Name is required"]},
-    occupation: {type: String, required: [true, "Occupation is required"]},
-    contactNo: {type: String, required: [true, "Contact number is required"]},
-    address: {type: String, required: [true, "Address is required"]}
+    name: {type: String, required: [true, "Name is required"], trim: true},
+    occupation: {type: String, required: [true, "Occupation is required"], trim: true},
+    contactNo: {type: String, required: [true, "Contact number is required"], trim: true},
+    address: {type: String, required: [true, "Address is required"], trim: true}
 });
 
 
@@ -40,10 +40,10 @@ const studentSchema = new Schema<Student>({
         },
         required: [true, "Gender is required"]
     },
-    dateOfBirth: {type: String},
+    dateOfBirth: {type: String, trim: true},
     email: {type: String, required: [true, "Email is required"], unique: true},
-    contactNo: {type: String, required: [true, "Contact number is required"]},
-    emergencyContactNo: {type: String, required: [true, "Emergency contact number is required"]},
+    contactNo: {type: String, required: [true, "Contact number is required"], trim: true},
+    emergencyContactNo: {type: String, required: [true, "Emergency contact number is required"], trim: true},
     bloodGroup: {
         type: String,
         enum: {
@@ -51,8 +51,8 @@ const studentSchema = new Schema<Student>({
             message: "{VALUE} is not a valid blood group"
         }
     },
-    presentAddress: {type: String, required: [true, "Present address is required"]},
-    permanentAddress: {type: String, required: [true, "Permanent address is required"]},
+    presentAddress: {type: String, required: [true, "Present address is required"], trim: true},
+    permanentAddress: {type: String, required: [true, "Permanent address is required"], trim: true},
     guardian: {
         type: guardianSchema,
         required: [true, "Guardian is required"]
